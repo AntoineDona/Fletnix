eb47349d69caeeb5057eb8174e03c56488abc095
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-10 03:15:50 +0200

    modif entrees db

--- backend/models/movie.js
+++ backend/models/movie.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
 const MovieSchema = new mongoose.Schema({
-  id_mdb: { type: Number, required: true, unique: true },
+  id_tmd: { type: Number, required: true, unique: true },
   name: { type: String },
   genres: { type: Array },
   release_date: { type: String },
   backdrop_path: { type: String },
   poster_path: { type: String },
   original_language: { type: String },
-  overview: { type: String }
-  original_title: { type: String},
+  overview: { type: String },
+  original_title: { type: String },
   production_companies: { type: Array },
   production_countries: { type: Array },
   spoken_languages: { type: Array },
   status: { type: String },
   tagline: { type: String },
-  vote_average: { type: Number, required : true, unique : true}
-  vote_count: { type: Number, required : true, unique : true},
+  vote_average: { type: Number },
+  vote_count: { type: Number },
 });
 
 const movieModel = mongoose.model("movieModel", MovieSchema, "movies");
