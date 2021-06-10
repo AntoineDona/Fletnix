2a0c54cf1f02722242800a90d815196c99930a2c
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-10 11:30:50 +0200

    search file

---
+++ frontend/src/views/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Top results for</h1>
+  <div class="counter-value">Counter value is: {{ counter }}</div>
+  <button @click="increment()">Increment</button>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment: function () {
+      this.counter++;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.counter-value {
+  margin-bottom: 5px;
+}
+</style>
