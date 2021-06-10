a48d633c6d51f8e478eb51ad9e63b52110f5b7f1
Fagard Leo <leo.fagard@student-cs.fr>
2021-06-10 09:32:42 +0200

    test

--- backend/models/movie.js
+++ backend/models/movie.js
@@ -8,15 +8,15 @@ const MovieSchema = new mongoose.Schema({
   backdrop_path: { type: String },
   poster_path: { type: String },
   original_language: { type: String },
-  overview: { type: String }
-  original_title: { type: String},
+  overview: { type: String },
+  original_title: { type: String },
   production_companies: { type: Array },
   production_countries: { type: Array },
   spoken_languages: { type: Array },
   status: { type: String },
   tagline: { type: String },
-  vote_average: { type: Number, required : true, unique : true}
-  vote_count: { type: Number, required : true, unique : true},
+  vote_average: { type: Number },
+  vote_count: { type: Number },
 });
 
 const movieModel = mongoose.model("movieModel", MovieSchema, "movies");
