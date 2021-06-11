751da0e32fb559435447ecb512f3cd70287d5e34
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-11 09:56:32 +0200

    others

--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -1,42 +1,18 @@
 <template>
-  <MovieDetails :movie="movie" />
+  <div></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MovieDetails from "@/views/MovieDetails.vue";
-import axios from "axios";
 
 export default {
   name: "Users",
-  components: {
-    MovieDetails,
-  },
   data: function () {
     return {
       movie: {},
       movieLoadingError: "",
     };
   },
-  methods: {
-    fetchMovie: function () {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/movie/337404?api_key=57359ff087905e870d40ba4880a1dce0`
-        )
-        .then((response) => {
-          this.movie = response.data.results;
-          console.log(this.movie);
-        })
-        .catch((error) => {
-          this.movieLoadingError = "An error occured while fetching movies.";
-          console.error(error);
-        });
-    },
-  },
-  created() {
-    console.log("count is: " + this.movieName);
-    this.fetchMovies();
-  },
+  created() {},
 };
 </script>
