444d14fb9a1eca289310d5d145121ca6cb8a66ca
Antoine Do Nascimento <antoinedn@live.fr>
2021-06-13 02:19:26 +0200

    search bar + correcting search bugs

--- frontend/src/views/Browse.vue
+++ frontend/src/views/Browse.vue
@@ -1,45 +1,60 @@
 <template>
   <div class="home">
-    <input
-      @keyup.enter="onChange"
-      v-model="movieName"
-      placeholder="Movie Research"
-    />
-    <ul id="array-rendering">
-      <li v-for="movie in movies" :key="movie.id">
-        <Movie :movie="movie" />
-      </li>
-    </ul>
+    <div class="search">
+      <h1>Rechercher</h1>
+      <div class="search_bar">
+        <input
+          @keyup.enter="onChange(movieName)"
+          v-model="movieName"
+          placeholder="Movie Research"
+        />
+        <button type="submit" class="search-btn">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+    <div class="results">
+      <div v-for="movie in movies" :key="movie.id" class="result_container">
+        <MovieResult :movie="movie" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Movie from "@/components/Movie.vue";
+import MovieResult from "@/components/MovieResult.vue";
 import axios from "axios";
 
 export default {
   name: "Browse",
   components: {
-    Movie,
+    MovieResult,
   },
   data: function () {
     return {
       movieName: "",
-      movies: [],
+      movies: {},
       usersLoadingError: "",
     };
   },
   watch: {
     // whenever question changes, this function will run
-    movieName: function (newMovie, oldMovie) {
-      this.getAnswer(newMovie);
+    movieName: function (newMovie) {
+      if (newMovie.length == 0) {
+        this.displayDefault();
+      } else {
+        this.getAnswer(newMovie);
+      }
     },
   },
   methods: {
-    /* onChange: function () {
-      window.location.href =
-        "http://localhost:3000/movies?name=" + this.movieName;
-    }, */
+    // onChange: function (newMovie) {
+    //   if (newMovie.length == 0) {
+    //     this.displayDefault();
+    //   } else {
+    //     this.getAnswer(newMovie);
+    //   }
+    // },
     /* fetchMovies: function () {
       axios
         .get(
@@ -55,16 +70,20 @@ export default {
         });
     }, */
     getAnswer: function (newMovie) {
+      // console.log(`http://localhost:3000/movies/search?name=${newMovie}`);
       axios
+        // .get(
+        //   `https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&language=fr&query=${newMovie}`
+        // )
         .get(
-          `https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${newMovie}`
+          `http://localhost:3000/movies/search?name=${newMovie}&sort_by=popularity`
         )
-        //.get(`http://localhost:3000/movies/search?name=${newMovie}`)
         .then((response) => {
           if (newMovie.length == 0) {
             return { message: `Enter a movies name` };
           } else {
-            this.movies = response.data.results;
+            console.log(response.data);
+            this.movies = response.data;
           }
           // Do something if call succe
         })
@@ -72,11 +91,25 @@ export default {
           this.movies = "Error! Could not reach the API. " + error;
         });
     },
+    displayDefault: function () {
+      axios
+        .get(
+          `http://localhost:3000/movies/?adult=false&sort_by=popularity&limit=20&status=Released`
+        )
+        .then((response) => {
+          this.movies = response.data;
+          console.log("premier if");
+        })
+        .catch((error) => {
+          this.moviesLoadingError = "An error occured while fetching movies.";
+          console.error(error);
+        });
+    },
   },
 
-  /* created() {
-    this.fetchMovies();
-  }, */
+  created() {
+    this.displayDefault();
+  },
   //watch: {
   // movieName: function (newName) {
   //   console.log(newName);
@@ -89,19 +122,47 @@ export default {
 <style scoped>
 .home {
   text-align: center;
-  padding-top: 5rem;
-  width: 10rem;
-  height: 10rem;
-  padding-bottom: 37rem;
-  padding-left: 45rem;
+  min-height: 82vh;
 }
 
-/* ul {
-  width: 100rem;
-  display: inline-flex;
-  align-content: center;
-} */
+h1 {
+  font-size: 2rem;
+}
+
 h3 {
   margin: 40px 0 0;
+}
+
+.results {
+  min-height: 30rem;
+}
+
+.search {
+  margin: 2rem 0;
+  display: flex;
+  justify-content: center;
+}
+
+.search_bar {
+  display: flex;
+  margin-left: 1rem;
+  width: auto;
+  background-color: white;
+  border: none;
+  padding: 0.4rem 0.6rem;
+  border-radius: 0.2rem;
+  flex-wrap: nowrap;
+}
+
+.search_bar input {
+  width: 15rem;
+  border: none;
+  outline: none;
+}
+
+.search-btn {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
 }
 </style>
